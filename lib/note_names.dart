c719ef15cb872181e8e602c2ab00af3210f54d28
mandersson1024 <mixand@gmail.com>
2019-11-10 21:20:57 +0100

    Flats and/or sharps.

--- lib/note_names.dart
+++ lib/note_names.dart
@@ -2,10 +2,19 @@
 class NoteNames {
 
   static const middleCNoteNumber = 60;
-  static const List<String> names = ["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"];
+  static const List<String> sharps = ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"];
+  static const List<String> flats = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"];
 
-  static String name(int note) {
-    return names[note % 12];
+  static String name(int note, {bool possiblySharp = true}) {
+    // todo: not sure how to handle flats and sharps
+    // todo: - set a key?
+    // todo: - show both alternatives?
+
+    if (possiblySharp) {
+      return sharps[note % 12];
+    } else {
+      return flats[note % 12];
+    }
   }
 
 }
