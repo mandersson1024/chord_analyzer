4354fee33affd8ae21ae2b2d440d3921c0b1483c
mandersson1024 <mixand@gmail.com>
2019-11-21 20:32:24 +0100

    Keyboard layout is now correct.

--- lib/keyboard_view.dart
+++ lib/keyboard_view.dart
@@ -2,18 +2,39 @@ import "dart:html";
 
 class KeyboardView {
 
-  static List<int>_positions = [0, 25, 40, 65, 80, 120, 145, 160, 185, 200, 225, 240];
+  static const int _whiteKeyWidth = 40;
+  static const int _blackKeyWidth = 25;
+  static const int _octaveWidth = _whiteKeyWidth * 7;
+  static List<int> _positions = [
+    0,
+    _blackKeyWidth,
+    _whiteKeyWidth,
+    _whiteKeyWidth + _blackKeyWidth,
+    2 * _whiteKeyWidth,
+    3 * _whiteKeyWidth,
+    3 * _whiteKeyWidth + _blackKeyWidth,
+    4 * _whiteKeyWidth,
+    4 * _whiteKeyWidth + _blackKeyWidth,
+    5 * _whiteKeyWidth,
+    5 * _whiteKeyWidth + _blackKeyWidth,
+    6 * _whiteKeyWidth,
+  ];
 
   static DivElement build() {
     DivElement container = DivElement();
 
-    for (int note = 48; note <= 83; ++note) {
+    int startNote = 48;
+    int numKeys = 36;
+    int startOffset = (startNote ~/ 12) * _octaveWidth;
+
+    for (int i = 0; i < numKeys; ++i) {
+      int note = startNote + i;
       int normalizedNote = note % 12;
       int octave = note ~/ 12;
 
       var key = _createKey(note)
         ..style.position = "absolute"
-        ..style.left = "${240 * octave + _positions[normalizedNote]}px"
+        ..style.left = "${_octaveWidth * octave + _positions[normalizedNote] - startOffset}px"
       ;
       container.children.add(key);
     }
